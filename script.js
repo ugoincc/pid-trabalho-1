e63152f48320d669c3e21bbfc3cf2d69decf88ec
renanBatalha <renan.batalha@gmail.com>
2025-05-16 17:44:28 -0300

    filtro da mediana reestruturado

--- script.js
+++ script.js
@@ -36,16 +36,15 @@ function handleImageFunction(selectedFunction) {
   preview.innerHTML = "";
   switch (selectedFunction) {
     case "fun1":
-      applyMedianFilter();
+      aplicarFiltroMediana();
       break;
     case "fun2":
-      applyRobertsFilter();
-      break;
-    case "fun3":
       
       break;
+    case "fun3":
+      break;
     case "fun4":
-      applySobelFilter()
+      //applySobelFilter()
       break;
     default:
       const para = document.createElement("p");
@@ -67,23 +66,22 @@ function createDownloadLink(canvas, filename = "imagem.png") {
 // Funcao para transformar o vetor de dados em uma matriz de pixels, onde cada pixel e um objeto com propriedades red, green, blue e gray
 // pre-condicao: data e um vetor de dados de imagem e width e height sao as dimensoes da imagem
 // pos-condicao: retorna uma matriz de pixels
-function transformDataVectorToMatrix(data, width, height) {
+function transformarVetorMatriz(data, largura, altura) {
   const matrix = [];
-  for (let i = 0; i < height; i++) {
+  for (let i = 0; i < altura; i++) {
     const row = [];
-    for (let j = 0; j < width; j++) {
+    for (let j = 0; j < largura; j++) {
 
-      const index = (i * width + j) * 4;
-      const red = data[index];
-      const green = data[index + 1];
-      const blue = data[index + 2];
-      const gray = 0.299 * red + 0.587 * green + 0.114 * blue;
-
+      const index = (i * largura + j) * 4;
+      const vermelho = data[index];
+      const verde = data[index + 1];
+      const azul = data[index + 2];
+      const opacidade = 255;
       const pixel = {
-        red: red,
-        green: green,
-        blue: blue,
-        gray: gray,
+        vermelho : vermelho,
+        verde : verde,
+        azul : azul,
+        opacidade : opacidade,
       };
 
       row.push(pixel);
@@ -108,76 +106,98 @@ function getMinMax(matrix) {
   return { min, max };
 }
 
-function findMedian(matrix, i, j) {
-  const values = [];
-  for (let x = -1; x < 2; x++) {
-    for (let y = -1; y < 2; y++) {
-      const pixel = matrix[i + x][j + y];
-      values.push(pixel.gray);
-    }
+function convertFloat32ToUint8Clamped(output){
+  const outputUint8Clamped = new Uint8ClampedArray(output.length);
+  for (let i = 0; i < output.length; i++) {
+    outputUint8Clamped[i] = output[i];
   }
-  values.sort((a, b) => a - b);
-  const median = Math.round(values[Math.floor(values.length / 2)]);
-  return median
+  return outputUint8Clamped;
 }
 
-function applyMedianFilter() {
+function encontrarMediana(matriz, i, j) {
+  const valores = [];
+  for (let y = 0; y < 3; y++) {
+    for (let x = 0; x < 3; x++) {
+      const pixel = matriz[i + y][j + x];
+      valores.push(pixel.vermelho);
+    }
+  }
+  valores.sort((a, b) => a - b);
+  const mediana = Math.round(valores[Math.floor(valores.length / 2)]);
+  return mediana;
+}
+
+// Revisao de Conceito Basico
+function aplicarEscalaCinza(vetorPixelsRGBA, pixelsConvertidos) {
+
+    // Convercao para Escala de Cinza
+    for(let i = 0; i < vetorPixelsRGBA.length; i += 4){
+      
+      valorEmCinza = 
+
+        vetorPixelsRGBA[i] * 0.299 +   // Vermelho
+
+        vetorPixelsRGBA[i + 1] * 0.587 + // Verde
+
+        vetorPixelsRGBA[i + 2] * 0.114 // Azul      
+
+      pixelsConvertidos[i] = pixelsConvertidos[i + 1] = pixelsConvertidos[i + 2] = valorEmCinza;
+
+      // Nao estamos trabalhando com opacidade
+      // Logo, configuramos para o valor maximo      
+      pixelsConvertidos[i + 3] = 255; // Opacity
+    }
+}
+
+function aplicarFiltroMediana() {
   const canvas = document.createElement("canvas");
   canvas.classList.add("styled-canva");
   const context = canvas.getContext("2d");
-  const img = new Image();
-  img.src = URL.createObjectURL(curFile);
+  const imagem = new Image();
+  imagem.src = URL.createObjectURL(curFile);
 
-  img.onload = () => {
-    canvas.width = img.width;
-    canvas.height = img.height;
-    context.drawImage(img, 0, 0);
-    const width = canvas.width;
-    const height = canvas.height;
-    const imageData = context.getImageData(0, 0, width, height);
-    const data = imageData.data;
-    const output = new Uint8ClampedArray(data.length);
-    const matrix = transformDataVectorToMatrix(data, width, height);
+  imagem.onload = () => {
+    canvas.width = imagem.width;
+    canvas.height = imagem.height;
+    context.drawImage(imagem, 0, 0);
+    const largura = canvas.width;
+    const altura = canvas.height;
+    const imageData = context.getImageData(0, 0, largura, altura);
+    const vetorPixelsRGBA = imageData.data;
+    const pixelsConvertidos = new Uint8ClampedArray(vetorPixelsRGBA.length);
 
-    for (let i = 1; i < height - 3; i++) {
-      for (let j = 1; j < width - 3; j++) {
-        const med = findMedian(matrix, i+1, j+1); 
-        console.log(med);
+    // -------------------- Convertendo o vetor extraido para escala de cinza -------------------- //
+    aplicarEscalaCinza(vetorPixelsRGBA, pixelsConvertidos);
 
-        const index = ((i+1) * width + (j+1)) * 4; // Corrigido para acessar o índice correto
-        output[index] = med;      // Atribuindo a mediana no valor R
-        output[index + 1] = med;  // Atribuindo a mediana no valor G
-        output[index + 2] = med;  // Atribuindo a mediana no valor B
-        output[index + 3] = 255;  // Mantendo o alfa como 255 (totalmente opaco)
+    // -------------------- A matriz recebe os pixels em escala de cinza ------------------------- //
+    const matriz = transformarVetorMatriz(pixelsConvertidos, largura, altura);
+
+    for (let i = 1; i < altura - 3; i++) {
+      for (let j = 1; j < largura - 3; j++) {
+
+        const mediana = encontrarMediana(matriz, i, j); 
+
+        console.log(mediana);
+        const index = ((i+1) * largura + (j+1)) * 4; 
+        pixelsConvertidos[index] = mediana;      
+        pixelsConvertidos[index + 1] = mediana;  
+        pixelsConvertidos[index + 2] = mediana;  
+        pixelsConvertidos[index + 3] = 255; // ignorando a opacidade 
+
+        console.log(pixelsConvertidos[index]);
+        console.log(pixelsConvertidos[index + 1]);
+        console.log(pixelsConvertidos[index + 2]);
+        console.log(pixelsConvertidos[index + 3]);
       }
     }
-    
-    /*
-    const outputMatrix = transformDataVectorToMatrix(output, width, height);
-    const { min: minGray, max: maxGray } = getMinMax(outputMatrix);
 
-    const divisor = maxGray - minGray || 1;
-    for (let i = 0; i < outputMatrix.length - 3; i++) {
-      for (let j = 0; j < outputMatrix[0].length- 3; j++) {
-        const gray = outputMatrix[i][j].gray;
-        const normalized = Math.round(((gray - minGray) / divisor) * 255);
-
-        const index = ((i+1) * width + (j+1)) * 4; // Corrigido para acessar o índice correto
-        output[index] = normalized;    // Atribuindo valor normalizado no canal R
-        output[index + 1] = normalized; // Atribuindo valor normalizado no canal G
-        output[index + 2] = normalized; // Atribuindo valor normalizado no canal B
-        output[index + 3] = 255;        // Mantendo o alfa como 255
-      }
-    }
-    */
-
-    const resultImage = new ImageData(output, width, height);
-    context.putImageData(resultImage, 0, 0);
+    const imagemResultante = new ImageData(pixelsConvertidos, largura, altura);
+    context.putImageData(imagemResultante, 0, 0);
     preview.appendChild(canvas);
 
     const downloadContainer = document.querySelector(".download-container");
     downloadContainer.innerHTML = "";
-    const downloadLink = createDownloadLink(canvas, "roberts_filtered.png");
+    const downloadLink = createDownloadLink(canvas, "imagem-resultante-mediana.png");
     downloadContainer.appendChild(downloadLink);
   };
 }
